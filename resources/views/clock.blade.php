55dd152cf6967ee44d3fee35cad04c63d6d765c7
billythekid <billy@the-kid.org>
2017-01-26 19:57:26 +0000

    spaceless/css/etc/

--- resources/views/clock.blade.php
+++ resources/views/clock.blade.php
@@ -6,38 +6,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Wrong Time Clock</title>
 <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet" type="text/css">
-<style>
-    html, body {
-        color:       #34323c;
-        font-family: 'Fugaz One', sans-serif;
-        font-weight: 100;
-        height:      100vh;
-        margin:      0;
-    }
-
-    .full-height {
-        height: 100vh;
-    }
-
-    .flex-center {
-        align-items:     center;
-        display:         flex;
-        justify-content: center;
-    }
-
-    .content {
-        text-align: center;
-    }
-
-    .title {
-        font-size: 50px;
-    }
-</style>
+<link rel="stylesheet" href="/css/app.css">
 <div class="flex-center full-height">
     <div class="content">
-        The time is not…
+        <span>The time is not…</span>
         <div class="title">
-            {!! $time->format('l jS \\of F Y <b\r>h:i:s A') !!}
+            <span>{!! $time->format('l jS \\of F Y <b\r>h:i:s A') !!}</span>
         </div>
         <div>
             <small>…it's actually {{ \Carbon\Carbon::now()->diffForHumans($time) }} that!</small>
