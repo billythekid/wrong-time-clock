59cbb78fe5560ee6e07ea92d8818dde4281efdda
billythekid <billy@the-kid.org>
2017-01-26 19:41:30 +0000

    title

--- resources/views/clock.blade.php
+++ resources/views/clock.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Wrong Time Clock</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet" type="text/css">
