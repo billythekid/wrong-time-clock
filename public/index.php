e5202c64fc8fa59a634386262770eee91a15fe3e
billythekid <billythekid@github.com>
2022-01-14 17:46:18 +0000

    I can't believe I previously used a whole laravel app for this, lol

--- public/index.php
+++ public/index.php
@@ -1,58 +1,25 @@
 <?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
-|
-*/
-
-require __DIR__.'/../bootstrap/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__.'/../vendor/autoload.php';
+use Carbon\Carbon;
+$randomOffset = random_int(1, 1439);
+$addOrSub     = ['add', 'sub'][random_int(0,1)];
+$time         = Carbon::now()->{$addOrSub . 'Minutes'}($randomOffset);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Wrong Time Clock</title>
+<style>@font-face{font-family:'Fugaz One';font-style:normal;font-weight:400;src:local("Fugaz One"),local("FugazOne-Regular"),url(https://fonts.gstatic.com/s/fugazone/v6/foh--NswBHNPbRHy7yEyyJBw1xU1rKptJj_0jans920.woff2) format("woff2");unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2212,U+2215}html,body{color:#34323c;font-family:'Fugaz One', sans-serif;font-weight:100;height:100vh;margin:0}.full-height{height:100vh}.flex-center{align-items:center;display:flex;justify-content:center}.content{text-align:center}.title{font-size:50px}</style><div class="flex-center full-height">
+    <div class="content">
+        <span>The time is not…</span>
+        <div class="title">
+            <span><?php echo $time->format('l jS \\of F Y<b\r>g:i:s A') ?></span>
+        </div>
+        <div>
+            <small>…it's actually <?php echo \Carbon\Carbon::now()->diffForHumans($time) ?> that!</small>
+        </div>
+    </div>
+</div>
+</html>
